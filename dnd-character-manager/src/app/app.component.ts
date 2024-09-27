0f603500a0c7a5b4a748784bb1da234630d7c1eb
Jdogwest <jdogwest000@gmail.com>
2024-09-28 00:06:50 +0300

    added header component and injected primeflex

--- dnd-character-manager/src/app/app.component.ts
+++ dnd-character-manager/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'dnd-character-manager';
+  show: boolean = false;
+  writeInConsole(event: string){
+    console.error(event);
+  }
 }
